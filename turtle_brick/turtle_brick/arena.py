2058065d32abeb53a9b7c9544481d86735e6d5c7
Ayush Gaggar <gaggarayush@gmail.com>
2022-10-13 14:02:58 -0500

    fixed rviz wall flickering

--- turtle_brick/turtle_brick/arena.py
+++ turtle_brick/turtle_brick/arena.py
@@ -17,14 +17,22 @@ from visualization_msgs.msg import Marker, MarkerArray
 from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
 from tf2_ros import TransformBroadcaster
 
+class State(Enum):
+    RUNNING = auto()
+    DROP_BRICK = auto()
+
 class Arena(Node):
     def __init__(self):
         super().__init__('arena')
+        self.count = 0
         self.timer = self.create_timer(1/250.0, self.timer_callback)
         self.marker_pub = self.create_publisher(Marker, "/wall_marker", 10)
+        self.brick_pub = self.create_publisher(Marker, "/brick_marker", 10)
         # self.marker_pub = self.create_publisher(MarkerArray, "/wall_marker", 10)
         self.brick_place = self.create_service(Place, "brick_place", self.place_callback)
-        
+        self.brick_place_client = self.create_client(Place, "brick_place")
+        self.state = State.RUNNING
+        self.broadcaster = TransformBroadcaster(self)
 
     def create_wall(self):
         self.marker_walls_border = Marker()
@@ -63,29 +71,39 @@ class Arena(Node):
         self.marker_brick.color.r = 132.0/255.0
         self.marker_brick.color.g = 31.0/255.0
         self.marker_brick.color.b = 39.0/255.0
-        self.marker_brick.color.a = 1.0
+        self.marker_brick.color.a = 0.0
         self.marker_brick.action = 0
-        
-        self.marker_brick.pose.position.x = 0.0
-        self.marker_brick.pose.position.y = 0.0
-        self.marker_brick.pose.position.z = 8.0
-        self.marker_brick.pose.orientation.x = 0.0
-        self.marker_brick.pose.orientation.y = 0.0
-        self.marker_brick.pose.orientation.z = 0.0
-        self.marker_brick.pose.orientation.w = 0.0
-        
-        self.marker_all = MarkerArray(markers = [self.marker_walls_border, self.marker_brick])
+        self.marker_brick.scale.x = 1.0
+        self.marker_brick.scale.y = 2.0
+        self.marker_brick.scale.z = 1.0
         
         return self.marker_walls_border
     
     def timer_callback(self):
-        self.marker_pub.publish(self.create_wall())
+        if (self.count%25) == 0:
+            self.marker_pub.publish(self.create_wall())
+        self.world_brick = TransformStamped()
+        self.world_brick.header.stamp = self.get_clock().now().to_msg()
+        self.world_brick.header.frame_id = "world"
+        self.world_brick.child_frame_id = "brick"
+        # world_brick.transform.translation.x = float(self.dx)
+        # world_brick.transform.rotation = angle_axis_to_quaternion(radians, [0, 0, -1.0])
+        
+        self.world_brick.transform.translation.x = self.marker_brick.pose.position.x
+        self.world_brick.transform.translation.y = self.marker_brick.pose.position.y
+        self.world_brick.transform.translation.z = self.marker_brick.pose.position.z
+        self.broadcaster.sendTransform(self.world_brick)
         self.g = 9.81
+        if self.state == State.DROP_BRICK:
+            self.brick_pub.publish(self.marker_brick)
+        self.count +=1
 
     def place_callback(self, request, response):
-        self.marker_brick.pose.x = request.place_x
-        self.marker_brick.pose.y = request.place_y
-        self.marker_brick.pose.z = request.place_z
+        self.state == State.DROP_BRICK
+        
+        self.marker_brick.pose.position.x = request.brick_x
+        self.marker_brick.pose.position.y = request.brick_y
+        self.marker_brick.pose.position.z = request.brick_z
         self.marker_brick.color.a = 1.0
         return response
 
--- turtle_brick/turtle_brick/turtle_robot.py
+++ turtle_brick/turtle_brick/turtle_robot.py
@@ -29,53 +29,26 @@ class TurtleRobot(Node):
         # Static broadcasters publish on /tf_static. We will only need to publish this once
         self.static_broadcaster = StaticTransformBroadcaster(self)
         self.broadcaster = TransformBroadcaster(self)
-        self.dx = 10  # used to control frame movement
-        # create the broadcaster
-        # self.static_broadcaster.sendTransform(self)
-        # self.broadcaster.sendTransform(self)
 
         world_base_tf = TransformStamped()
         world_base_tf.header.stamp = self.get_clock().now().to_msg()
         world_base_tf.header.frame_id = "world"
-        world_base_tf.child_frame_id = "base"
+        world_base_tf.child_frame_id = "odom"
+        world_base_tf.transform.translation.x = 5.0
+        world_base_tf.transform.translation.y = 5.0
         self.static_broadcaster.sendTransform(world_base_tf)
 
-        base_odom = TransformStamped()
-        base_odom.header.stamp = self.get_clock().now().to_msg()
-        base_odom.header.frame_id = "base"
-        base_odom.child_frame_id = "odom"
-        # base_odom.transform.translation.x = -float(self.dx)
-        # get a quaternion corresponding to a rotation by theta about an axis
-        self.static_broadcaster.sendTransform(base_odom)
         # Create a timer to do the rest of the transforms
-
         self.tmr = self.create_timer(1, self.timer_callback)
 
     def timer_callback(self):
-
-        # Now create the transform, noted that it must have a parent frame and a timestamp
-        # The header contains the timing information and frame id
-
-        base_brick = TransformStamped()
-        base_brick.header.stamp = self.get_clock().now().to_msg()
-        base_brick.header.frame_id = "base"
-        base_brick.child_frame_id = "brick"
-        # base_brick.transform.translation.x = float(self.dx)
-        # base_brick.transform.rotation = angle_axis_to_quaternion(radians, [0, 0, -1.0])
-        self.broadcaster.sendTransform(base_brick)
-
-        base_base_link = TransformStamped()
-        base_base_link.header.stamp = self.get_clock().now().to_msg()
-        base_base_link.header.frame_id = "base"
-        base_base_link.child_frame_id = "base_link"
-        # base_base_link.transform.translation.x = float(self.dx)
-        # base_base_link.transform.rotation = angle_axis_to_quaternion(radians, [0, 0, -1.0])
-        self.broadcaster.sendTransform(base_base_link)
-
-        # update the movement
-        self.dx -= 1
-        if self.dx == 0:
-            self.dx = 10
+        odom_base_link = TransformStamped()
+        odom_base_link.header.stamp = self.get_clock().now().to_msg()
+        odom_base_link.header.frame_id = "odom"
+        odom_base_link.child_frame_id = "base_link"
+        # odom_base_link.transform.translation.x = float(self.dx)
+        # odom_base_link.transform.rotation = angle_axis_to_quaternion(radians, [0, 0, -1.0])
+        self.broadcaster.sendTransform(odom_base_link)
 
 
 def main(args=None):

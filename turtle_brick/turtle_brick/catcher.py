1bdff012d31c2cbd64f55e7e2d94c89ce83a915b
Ayush Gaggar <gaggarayush@gmail.com>
2022-10-15 17:01:20 -0500

    catcher text marker

--- turtle_brick/turtle_brick/catcher.py
+++ turtle_brick/turtle_brick/catcher.py
@@ -35,30 +35,37 @@ class Catcher(Node):
         self.goal_sub = self.create_subscription(Point, "goal_message", self.goal_move_callback, 1)
         self.current_pos = Pose(x=0.0,y=0.0,theta=0.0,linear_velocity=0.0,angular_velocity=0.0)
         self.pos_or_subscriber = self.create_subscription(Pose, "turtlesim/turtle1/pose", self.pos_or_callback, 10)
-        
+        self.reachable_pub = self.create_publisher(Marker, "/text_marker", 10)
+
+        self.text_reachable = Marker(type=9)        
 
     def timer_callback(self):
-
+        if self.state == State.BRICK_PUBLISHED and self.reachable == False:
+            self.reachable_pub.publish(self.text_reachable)
         return
     
     def goal_move_callback(self, msg):
         goal = msg
-        t_req = math.sqrt(2*(self.platform_height-self.goal.z)/self.g)
-        goal_distance = math.sqrt((goal.y-self.current_pos.y)**2+(goal.x-self.current_pos.x)**2)
+        if (self.platform_height-goal.z) >= 0:
+            t_req = math.sqrt(2*(self.platform_height-goal.z)/self.g)
+        else:
+            t_req = 0.0
+        distance_goal = math.sqrt((goal.y-self.current_pos.y)**2+(goal.x-self.current_pos.x)**2)
+        height_goal = goal.z - self.platform_height
         
         self.state = State.BRICK_PUBLISHED
-        if goal_distance/self.max_velocity <= t_req:
+        if distance_goal/self.max_velocity <= t_req and height_goal >= .5*self.g*t_req**2:
             self.reachable = True
-        else:
-            text_reachable = Marker(type=9)
-            text_reachable.header.frame_id = "platform_tilt"
-            text_reachable.header.stamp = self.get_clock().now().to_msg()
-            text_reachable.action = 0
-            text_reachable.scale.z = self.wheel_radius*2.5
-            text_reachable.id = 10
-            text_reachable.pose = Pose(x=0.0,y=0.0,z=self.wheel_radius)
-            text_reachable.string = "Unreachable"
-            text_reachable.lifetime = Duration(sec=3.0,nanosec=0.0)
+        else:    
+            self.text_reachable.header.frame_id = "platform_tilt"
+            self.text_reachable.header.stamp = self.get_clock().now().to_msg()
+            self.text_reachable.action = 0
+            self.text_reachable.scale.z = self.wheel_radius*2.5
+            self.text_reachable.id = 10
+            self.text_reachable.pose._position.z = self.wheel_radius
+            self.text_reachable.string = "Unreachable"
+            self.text_reachable.lifetime = Duration(sec=3.0,nanosec=0.0)
+            self.text_reachable.color.a = 1.0
         return
     
     def pos_or_callback(self,msg):
